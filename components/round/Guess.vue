b0c9897dee072f7471fb9b680788df2fafbfae28
Santeri Nogelainen <santeri.nogelainen@kehatieto.fi>
2023-12-28 00:08:00 +0200

    Ei saa arvata toteutus

--- components/round/Guess.vue
+++ components/round/Guess.vue
@@ -6,12 +6,25 @@
   <v-btn @click="unguessNumber">Takaisin</v-btn>
   <v-btn
     v-for="n in options"
+    :disabled="isDisabled(n)"
     @click="() => guessNumber(n)"
     >{{ n }}</v-btn
   >
 </template>
 
 <script setup lang="ts">
+import game from '~/logic/game';
 import { guesser, guessNumber, unguessNumber } from '~/logic/guess';
-import { options } from '~/logic/round';
+import round, { options } from '~/logic/round';
+
+const isDisabled = (option: number) => {
+  // last player is guessing and more than 4 cards are in play
+  if (round.value.guesses.length === game.value.playerCount && round.value.cards >= 4) {
+    // this is to guarantee that someone guesses incorrectly
+    const sum = round.value.guesses.reduce<number>((total, guess) => total + (guess.guess ?? 0), 0);
+    const diff = round.value.cards - sum;
+    return diff === option;
+  }
+  return false;
+}
 </script>
