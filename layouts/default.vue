06ba9c1b83fd8304448b79f0fceab6d19fcdccc3
Santeri Nogelainen <santeri.nogelainen@kehatieto.fi>
2023-12-28 15:17:17 +0200

    Seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,10 @@
 <template>
+  <Title>{{ title }}</Title>
+  <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <Link rel="manifest" href="/site.webmanifest" />
+
   <v-app>
     <v-app-bar :elevation="2">
       <v-app-bar-nav-icon
@@ -7,9 +13,6 @@
       ></v-app-bar-nav-icon>
       <v-app-bar-title>
         {{ title }}
-        <Title>
-          {{ title }}
-        </Title>
       </v-app-bar-title>
     </v-app-bar>
 
